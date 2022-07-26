cb3a393b5533ce0899e0c87438385eda318de991
John Thompson <john@springframework.guru>
2022-07-26 15:49:37 -0400

    Initial Project Add

---
+++ src/main/java/guru/springframework/spring6di/Spring6DiApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6di;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6DiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/guru/springframework/spring6di/Spring6DiApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6di;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6DiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
