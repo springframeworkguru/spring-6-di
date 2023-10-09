7490f854fa11fed6f1b35ab0cb24867beb6552b7
PichaevDmitriyExpera <89770524+PichaevDmitriyExpera@users.noreply.github.com>
2023-10-09 11:28:00 +0500

    add primary implementation

--- src/test/java/guru/springframework/spring6di/controllers/PropertyInjectedControllerTest.java
+++ src/test/java/guru/springframework/spring6di/controllers/PropertyInjectedControllerTest.java
@@ -1,13 +1,11 @@
 package guru.springframework.spring6di.controllers;
 
-import guru.springframework.spring6di.services.*;
 import org.junit.jupiter.api.*;
-import static org.junit.jupiter.api.Assertions.*;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.boot.test.context.*;
 
 @SpringBootTest
-class propertyInjectedControllerTest {
+class PropertyInjectedControllerTest {
 
     @Autowired
     PropertyInjectedController propertyInjectedController;
